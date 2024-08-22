15c4babd073c0bfa89b592ddeefc0cc5bb4c249e
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-22 22:05:20 +0500

    Create register func

--- pages/page/auth.py
+++ pages/page/auth.py
@@ -17,9 +17,27 @@ class Auth:
         pass
 
     @log_decorator
+    def create_user_table(self):
+        query = '''
+        CREATE TABLE IF NOT EXISTS USERS (
+        ID SERIAL PRIMARY KEY,
+        FIRSTNAME VARCHAR(255) NOT NULL,
+        LASTNAME VARCHAR(255) NOT NULL,
+        EMAIL VARCHAR(255) NOT NULL UNIQUE,
+        PASSWORD VARCHAR(256) NOT NULL,
+        IS_LOGIN BOOLEAN DEFAULT FALSE,
+        CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+        '''
+        with self.__database as cursor:
+            cursor.execute(query)
+        return None
+
+    @log_decorator
     def register(self):
-        query = sql.SQL("SELECT * FROM {}").format(sql.Identifier('users'))
-        print(execute_query(query, fetch='all'))
+        self.create_user_table()
+        query = sql.SQL("SELECT * FROM {};").format(sql.Identifier('users'))
+        print(execute_query(query))
         print("Table created successfully")
         first_name: str = input("First Name: ")
         last_name: str = input("Last Name: ")
@@ -30,7 +48,20 @@ class Auth:
             print("Passwords do not match")
             password: str = hashlib.md5(input("Password: ").strip().encode('utf-8')).hexdigest()
             confirm_password: str = hashlib.md5(input("Confirm password: ").strip().encode('utf-8')).hexdigest()
+        query = '''
+               INSERT INTO "users" (FIRSTNAME, LASTNAME, EMAIL, PASSWORD)
+               VALUES (%s, %s, %s, %s);
+               '''
+        params = (first_name, last_name, email, password)
+
+        with self.__database as db:
+            db.execute(query, params)
+        print("User registered successfully")
+        return None
 
     @log_decorator
     def logout(self):
-        pass
+        email: str = input("Email: ").strip()
+        password: str = hashlib.md5(input("Password: ").strip().encode('utf-8')).hexdigest()
+        user = execute_query("SELECT * FROM users WHERE EMAIL=%s", email)
+        print(user)
