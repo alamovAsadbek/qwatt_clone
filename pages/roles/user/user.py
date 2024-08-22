720a6ec8b4847c191f0aeacc7b4e2c0378365923
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-22 23:29:58 +0500

    Change files

--- pages/roles/user/user.py
+++ pages/roles/user/user.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from psycopg2 import sql
 
 from main_files.database.db_setting import Database, get_active_user
@@ -7,10 +9,19 @@ from main_files.decorator.decorator_func import log_decorator
 class User:
     def __init__(self):
         self.__database = Database()
+        self.__date_now = datetime.now().strftime('%m/%d/%Y %H:%M:%S')
 
     @log_decorator
     def rent_products(self, product_name: str):
-        pass
+        self.create_rent_table()
+        active_user = get_active_user()
+        query = sql.SQL('''
+        INSERT INTO RENTALS (USER_EMAIL, RENT_PRODUCT)
+        VALUES (%s, %s);
+        ''')
+        params = (active_user['email'], product_name)
+        with self.__database as cursor:
+            cursor.execute(query, params)
 
     @log_decorator
     def create_rent_table(self):
@@ -20,8 +31,8 @@ class User:
         PRODUCT_ID SERIAL UNIQUE,
         USER_EMAIL VARCHAR(255) NOT NULL,
         RENT_PRODUCT VARCHAR(255) NOT NULL,
-        RENT_TIME DEFAULT CURRENT_TIMESTAMP,
-        STATUS BOOLEN NOT NULL DEFAULT FALSE,
+        RENT_TIME DATE DEFAULT CURRENT_TIMESTAMP,
+        STATUS BOOLEAN NOT NULL DEFAULT FALSE
         );
         ''')
         with self.__database as cursor:
@@ -31,17 +42,6 @@ class User:
     # rent a bike
     @log_decorator
     def rent_bicycle(self):
-        self.create_rent_table()
-        active_user = get_active_user()
-        query = sql.SQL('''
-        INSERT INTO RENTALS(
-        USER_EMAIL {user_email},
-        RENT_PRODUCT {product_name}
-        );
-        ''').format(
-            user_email=active_user['email'],
-            product_name=sql.Identifier('bicycle')
-        )
-        with self.__database as cursor:
-            cursor.execute(query)
+        self.rent_products(product_name='bicycle')
+        print(f"You rented a bike at {self.__date_now}")
         return True
