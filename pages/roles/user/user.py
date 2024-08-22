8a011d2e4e7349d61afba6d3ce7a4e564a740827
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-22 22:46:31 +0500

    create create_rent_table func in user.py

--- pages/roles/user/user.py
+++ pages/roles/user/user.py
@@ -2,6 +2,10 @@ from main_files.decorator.decorator_func import log_decorator
 
 
 class User:
+    @log_decorator
+    def create_rent_table(self):
+        pass
+
     # rent a bike
     @log_decorator
     def rent_bicycle(self):
