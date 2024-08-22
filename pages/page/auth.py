74f758498040ee91b04e009b65c49d04b5c99e78
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-22 12:43:15 +0500

    Change register func

--- pages/page/auth.py
+++ pages/page/auth.py
@@ -1,14 +1,28 @@
+import datetime
+import hashlib
+
 from main_files.decorator.decorator_func import log_decorator
 
 
 class Auth:
+    def __init__(self):
+        self.created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S").__str__()
+
     @log_decorator
     def login(self):
         pass
 
     @log_decorator
     def register(self):
-        pass
+        first_name: str = input("First Name: ")
+        last_name: str = input("Last Name: ")
+        email: str = input("Email: ")
+        password: str = hashlib.md5(input("Password: ").strip().encode('utf-8')).hexdigest()
+        confirm_password: str = hashlib.md5(input("Confirm password: ").strip().encode('utf-8')).hexdigest()
+        while password != confirm_password:
+            print("Passwords do not match")
+            password: str = hashlib.md5(input("Password: ").strip().encode('utf-8')).hexdigest()
+            confirm_password: str = hashlib.md5(input("Confirm password: ").strip().encode('utf-8')).hexdigest()
 
     @log_decorator
     def logout(self):
