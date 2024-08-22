ca2e2f4a356aadbf0bf8c0e17cc25b8de2bf43d5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-22 22:49:09 +0500

    Change user.py

--- pages/roles/user/user.py
+++ pages/roles/user/user.py
@@ -1,10 +1,27 @@
+from main_files.database.db_setting import Database
 from main_files.decorator.decorator_func import log_decorator
 
 
 class User:
+    def __init__(self):
+        self.__database = Database()
+
     @log_decorator
     def create_rent_table(self):
-        pass
+        query='''
+        CREATE TABLE IF NOT EXISTS RENTALS (
+        ID SERIAL PRIMARY KEY,
+        FIRSTNAME VARCHAR(255) NOT NULL,
+        LASTNAME VARCHAR(255) NOT NULL,
+        EMAIL VARCHAR(255) NOT NULL UNIQUE,
+        PASSWORD VARCHAR(256) NOT NULL,
+        IS_LOGIN BOOLEAN DEFAULT FALSE,
+        CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+        '''
+        with self.__database as cursor:
+            cursor.execute(query)
+        return True
 
     # rent a bike
     @log_decorator
